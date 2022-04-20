d46f7b316f387dfba02f5593ef39fec5fc290461
Juan Camilo Botero Zapata <juan_camilobotero@hootmail.com>
2022-04-19 21:00:44 -0500

    asi por ahora

--- front.js
+++ front.js
@@ -2,13 +2,26 @@
 
 const selector1 = document.querySelector(".TdV");
 const selector2 = document.querySelector(".selector2");
+const displayTablas = document.querySelector(".displayTablas")
 
 selector1.addEventListener("change",()=>{
     const selector2 = document.querySelector(".selector2");
     selector2.style.display = 'block';
 })
+selector2.addEventListener("change",()=>{
+    displayTablas.style.display = 'block';
 
-const baseTabla = [
+const baseTabla1 = [
+    ['1','','',''],
+    ['2','','',''],
+    ['3','','',''],
+    ['4','','',''],
+    ['5','','',''],
+    ['6','','',''],
+    ['7','','',''],
+];
+
+const baseTabla2 = [
     ['1','','',''],
     ['2','','',''],
     ['3','','',''],
@@ -19,24 +32,33 @@ const baseTabla = [
 ];
 
 const tabla1 = document.querySelector('.tabla1');
+const tabla2 = document.querySelector('.tabla2');
 
-const hot = new Handsontable(tabla1, {
-    data: baseTabla,
+const hot1 = new Handsontable(tabla1, {
+    data: baseTabla1,
     colHeaders: ['Rango','Limite inferior','Limite Superior','Prob %'],
     height: 'auto',
     licenseKey: 'non-commercial-and-evaluation',
     cells(row,col) {
-        if(col==0 || col==4 || row==7)
+        if(col==0)
         return{
             readOnly: true,
         };
     },
-    afterCreateRow: function(index,numberOfRow){
-        baseTabla.splice(index,numberOfRow)
-    },
-    width: "auto",
-    height: 'auto'
+   width: "100%"
 })
 
-
-
+const hot2 = new Handsontable(tabla2, {
+    data: baseTabla2,
+    colHeaders: ['Rango','Limite inferior','Limite Superior','Prob %'],
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation',
+    cells(row,col) {
+        if(col==0)
+        return{
+            readOnly: true,
+        };
+    },
+   width: "100%"
+})
+})
